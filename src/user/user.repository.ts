d7125282095ec1f90e2b3b0d9f88162aaf9e35f8
Pham Tuan Khoi <huynhanhpham734@gmail.com>
2024-06-18 22:02:57 +0700

    add repository and baseAbstractClass

---
+++ src/user/user.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { FilterQuery, Model } from 'mongoose';
+import { User, UserDocument } from './schema/user.schema';
+import { BaseAbstractRepository } from 'src/database/base.abstract.repository';
+
+@Injectable()
+export class UserRepository extends BaseAbstractRepository<UserDocument> {
+  constructor(
+    @InjectModel(User.name) model: Model<UserDocument>,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {
+    super(model);
+  }
+
+  findById(id: string) {
+    return this.userModel.findById(id).lean();
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email }).select('+password').lean();
+  }
+}
